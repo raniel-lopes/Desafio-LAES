48228ef4c72c2172b17fa7b97dee53e444f3bca6
Raniel Lopes <raniel.ldms@gmail.com>
2025-08-24 23:39:29 -0300

    Update missingNumber.js

--- ex09/missingNumber.js
+++ ex09/missingNumber.js
@@ -10,7 +10,7 @@ function missingNumber(nums) {
     return somaEsperada - somaReal;
 }
 
-// Exemplos:
+// Teste
 console.log(missingNumber([3, 0, 1]));
 console.log(missingNumber([0, 1]));
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));    
